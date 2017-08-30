21dfc8b754bfb79fce86f7649b019502e9707eea
Yuliya <ylobukhova@gmail.com>
2017-08-31 00:55:21 +0300

    Изменение цветов плащей и глаз. Исправление 2

--- js/setup.js
+++ js/setup.js
@@ -5,8 +5,6 @@ var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария
 
 var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-var WIZARD_NAMES;
-
 var WIZARD_COATCOLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
 var WIZARD_EYESCOLOR = ['black', 'red', 'blue', 'yellow', 'green'];
@@ -20,7 +18,7 @@ var similarListElement = userDialog.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var getRandomObject = function (name, coatColor, eyesColor, count) {
+var getRandomObject = function (count) {
   var wizardsArray = [];
   for (var i = 0; i < count; i++) {
     wizardsArray[i] = {
@@ -48,7 +46,7 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var wizards = getRandomObject(WIZARD_NAMES, WIZARD_COATCOLOR, WIZARD_EYESCOLOR, WIZARDS_COUNT);
+var wizards = getRandomObject(WIZARDS_COUNT);
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
